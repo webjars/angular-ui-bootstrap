d7608e20e899d3918e3bb350549ad5fc4d050e2c
James Ward <james@jamesward.org>
2014-03-08 11:01:31 -0700

    cleanup

--- src/main/resources/webjars-requirejs.js
+++ src/main/resources/webjars-requirejs.js
@@ -1,12 +1,10 @@
-/*global requirejs */
-
-// Ensure any request for this webjar brings in dependencies.
 requirejs.config({
     paths: {
         'ui-bootstrap': webjars.path('angular-ui-bootstrap', 'ui-bootstrap'),
         'ui-bootstrap-tpls': webjars.path('angular-ui-bootstrap', 'ui-bootstrap-tpls')
     },
     shim: {
-        'ui-bootstrap': [ 'angular' ]
+        'ui-bootstrap': [ 'angular' ],
+        'ui-bootstrap-tpls': [ 'angular' ]
     }
 });
